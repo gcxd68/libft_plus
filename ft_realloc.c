a8fb91e2db693a39683d03a5b60d4e0292dc16ee
gcxd68 <94600257+gcxd68@users.noreply.github.com>
2025-03-27 16:04:28 +0100

    Add files via upload

--- ft_realloc.c
+++ ft_realloc.c
@@ -19,7 +19,11 @@ void	*ft_realloc(void *ptr, size_t old_size, size_t new_size)
 	if (!ptr)
 		return (malloc(new_size));
 	if (new_size == 0)
-		return (free(ptr), NULL);
+	{
+		free(ptr);
+		ptr = NULL;
+		return (NULL);
+	}
 	new_ptr = malloc(new_size);
 	if (!new_ptr)
 		return (NULL);
@@ -27,5 +31,7 @@ void	*ft_realloc(void *ptr, size_t old_size, size_t new_size)
 		ft_memcpy(new_ptr, ptr, old_size);
 	else
 		ft_memcpy(new_ptr, ptr, new_size);
-	return (free(ptr), new_ptr);
+	free(ptr);
+	ptr = NULL;
+	return (new_ptr);
 }
